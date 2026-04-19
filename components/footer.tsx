c7740d56e074b5cc6139025089cdda9bce9a3e30
pranavxiyer <pranavxiyer@gmail.com>
2026-04-19 17:30:22 -0500

    open links in a new tab

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,11 +3,11 @@ export default function Footer() {
         <div className="flex gap-2">
           <a href="mailto:pranavxiyer@u.northwestern.edu">Email</a>
           <span>·</span>
-          <a href="https://github.com/pranavxiyer">GitHub</a>
+          <a href="https://github.com/pranavxiyer" target="_blank" rel="noopener noreferrer">GitHub</a>
           <span>·</span>
-          <a href="https://x.com/pranavxiyer">X</a>
+          <a href="https://x.com/pranavxiyer" target="_blank" rel="noopener noreferrer">X</a>
           <span>·</span>
-          <a href="https://www.linkedin.com/in/pranavxiyer/">LinkedIn</a>
+          <a href="https://www.linkedin.com/in/pranavxiyer/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
         </div>
     )
 }
