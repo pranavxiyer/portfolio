a235b8b2e0166d4092b9d2cdc7004b22fdd50748
pranavxiyer <pranavxiyer@gmail.com>
2026-04-22 16:29:41 -0500

    redirect links + innovate

--- components/about.tsx
+++ components/about.tsx
@@ -1,10 +1,11 @@
 export default function About() {
     return (
         <div className="space-y-2">
-          <p>I'm a computer science graduate student at <a href="https://www.northwestern.edu/">Northwestern University</a>.</p>
-
-          <p>I completed my undergraduate studies in computer science at the <a href="https://www.berkeley.edu">University of California, Berkeley</a>, 
-            where I was affiliated with the <a href="https://www.humansleepscience.com/">Center for Human Sleep Science</a>.</p>
+            <p>I'm a computer science graduate student at <a href="https://www.northwestern.edu/" target="_blank" rel="noopener noreferrer">Northwestern University</a>.</p>
+            
+            <p>I completed my undergraduate studies in computer science at the <a href="https://www.berkeley.edu" target="_blank" rel="noopener noreferrer">University of California, Berkeley</a>, 
+            where I was affiliated with the <a href="https://www.humansleepscience.com/" target="_blank" rel="noopener noreferrer">Center for Human Sleep Science</a> and 
+            helped run <a href="https://berkeleyinnovate.com/" target="_blank" rel="noopener noreferrer">Innovate @ Berkeley</a>.</p>
         </div>
     )
 }
